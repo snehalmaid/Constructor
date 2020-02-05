9313b748d8d920ca3a113117ccd9a3cb3b75f9e1
snehalmaid <snehalmaid@mail.usf.edu>
2020-02-04 20:52:16 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Constructor
+{
+    class Car
+    {
+        public string model;  // Create a field
+
+        // Create a class constructor for the Car class
+        public Car()
+        {
+            model = "Mustang"; // Set the initial value for model
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    Car Ford = new Car();  // Create an object of the Car Class (this will call the constructor)
+        //    Console.WriteLine(Ford.model);  // Print the value of model
+        //}
+    }
+    class Car1
+    {
+        public string model;
+
+        // Create a class constructor with a parameter
+        public Car1(string modelName)
+        {
+            model = modelName;
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    Car1 Ford = new Car1("Mustang");
+        //    Console.WriteLine(Ford.model);
+        //}
+    }
+    class example
+    {
+        // In-accessibility due to class being private
+        //static void Main(string[] args)
+        //{
+        //    Car1 newob = new Car1("Ferari");
+        //    Console.WriteLine(newob.model);
+
+        //}
+    }
+}
